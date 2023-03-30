017ae4bd6b691b4fa28e7dcfe5807a7e3d46fb65
Nicolás Maximiliano Caminos <nicoasis7@gmail.com>
2023-03-30 12:50:21 -0300

    0.01

---
+++ booleanos/bool.js
@@ -0,0 +1,11 @@
+// los booleanos son valores primitivos que dan dos valores: TRUE (Verdadero), FALSE(Falso)
+let verdadero = true;
+
+let falso = false;
+
+console.log(verdadero, falso);
+console.log(typeof verdadero, typeof falso);
+console.log(Boolean(0));
+console.log(Boolean(-7));
+console.log(Boolean(" "));
+s;
