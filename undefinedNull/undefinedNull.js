01074b3833cc2a42ed97574bbf097068ca5db405
Nicolás Maximiliano Caminos <nicoasis7@gmail.com>
2023-04-03 11:29:03 -0300

    undefined, null & NaN

---
+++ undefinedNull/undefinedNull.js
@@ -0,0 +1,12 @@
+//Undefined respresentan valores ausentes, es una variable que no fue inicializada.
+
+let indefinida;
+console.log(indefinida);
+
+//Null ha sido una variable iniciada con el valor nulo.
+let nulo = null;
+console.log(nulo);
+
+//NaN es una operación que no es un numero
+let noEsUnNumero = "hola" * 54;
+console.log(noEsUnNumero);
